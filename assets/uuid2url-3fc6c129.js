dc07c36fb55677527171aecfe47dbcf83ec9549d
Jason <tenaxlo@tenaxdeair.home>
2025-07-29 12:20:27 -0400

    release

--- assets/uuid2url-3fc6c129.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index-2c2925e9.js";const d=async a=>(await r({url:`/api/admin/fadada/download-url?uuid=${a}`,method:"get"})).data;export{d as u};
---
+++ assets/uuid2url-fe9c9b11.js
@@ -0,0 +1,1 @@
+import{r}from"./index-36afcd41.js";const d=async a=>(await r({url:`/api/admin/fadada/download-url?uuid=${a}`,method:"get"})).data;export{d as u};
