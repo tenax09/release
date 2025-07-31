8c5a0be24678e8bfbd894c66e7c29794649ded7e
Jason <tenaxlo@tenaxdeMacBook-Air.local>
2025-07-30 22:59:34 -0400

    release

---
+++ assets/uuid2url-e312ab3b.js
@@ -0,0 +1,1 @@
+import{r}from"./index-31409f0b.js";const d=async a=>(await r({url:`/api/admin/fadada/download-url?uuid=${a}`,method:"get"})).data;export{d as u};
--- assets/uuid2url-ff953488.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index-b0a30ee6.js";const d=async a=>(await r({url:`/api/admin/fadada/download-url?uuid=${a}`,method:"get"})).data;export{d as u};
