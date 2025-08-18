2032902f85487276a419196044ac3ce97788a87b
Jason <tenaxlo@tenaxdeair.home>
2025-08-18 15:23:52 -0400

    release

--- assets/log-cfb777be.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-5e7125b4.js";function a(o){return t({url:"/api/admin/logs",method:"get",params:o})}function e(o){return t({url:"/api/admin/logs",method:"post",data:o})}export{e as a,a as g};
