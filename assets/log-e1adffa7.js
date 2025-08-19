22baeee9a8b52e48d4f1053b7d4c0aa45ab7e4af
Jason <tenaxlo@tenaxdeair.home>
2025-08-18 23:08:09 -0400

    release

---
+++ assets/log-e1adffa7.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-565bf4a8.js";function a(o){return t({url:"/api/admin/logs",method:"get",params:o})}function e(o){return t({url:"/api/admin/logs",method:"post",data:o})}export{e as a,a as g};
